13c19ed8cc4ec2eead33f382b39068d420a4a5c7
ayush9301 <ayushsolanki9301@gmail.com>
2024-12-15 22:18:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,79 @@
+
+const cont = document.getElementById("maincontainer");
+cont.style.backgroundColor = "#D9D9D9";
+cont.style.height = "550px";
+cont.style.display = "flex";
+cont.style.alignItems = "center";
+cont.style.justifyContent = "center";
+
+console.log(cont);
+
+const mcard = document.getElementById("main-card");
+mcard.style.backgroundColor = "#8900FF";
+mcard.style.height = "400px";
+mcard.style.width = "270px";
+mcard.style.display = "flex";
+mcard.style.alignItems = "end";
+mcard.style.justifyContent = "center";
+// mcard.style.border = " solid #0000ff"
+mcard.style.borderRadius = "15px";
+
+console.log(mcard);
+
+const card = document.getElementById("card");
+card.style.backgroundColor = "white";
+card.style.height = "265px";
+card.style.width = "285px";
+card.style.display = "flex";
+card.style.alignItems = "center";
+card.style.justifyContent = "center";
+card.style.flexDirection = "column";
+// card.style.border = " solid #0000ff"
+card.style.borderBottomRightRadius = "15px";
+card.style.borderBottomLeftRadius = "15px";
+// card.style.justifyContent = "space-around"
+
+console.log(card);
+
+const image = document.getElementById("img");
+image.style.height = "100px";
+image.style.width = "100px";
+image.style.borderRadius = "90px";
+image.style.position = "relative";
+image.style.bottom = "15px";
+image.style.border = " 3px solid #FFFFF3";
+
+console.log(image);
+
+const h1 = document.getElementById("h1");
+h1.style.position = "relative";
+h1.style.bottom = "30px";
+h1.style.fontSize = "25px";
+
+console.log(h1);
+
+const h2 = document.getElementById("h2");
+h2.style.position = "relative";
+h2.style.bottom = "60px";
+h2.style.color = "#B85CBF";
+
+console.log(h2);
+
+const para = document.getElementById("para");
+para.style.position = "relative";
+para.style.bottom = "80px";
+para.style.textAlign = "center";
+console.log(para);
+
+const btn = document.getElementById("btn");
+btn.style.backgroundColor = "#8900FF";
+btn.style.position = "relative";
+btn.style.bottom = "75px";
+btn.style.color = "white";
+btn.style.borderRadius = "15px";
+btn.style.border = "none";
+btn.style.height = "60px";
+btn.style.padding = "7px";
+btn.style.paddingInline = "20px";
+
+console.log(btn);
